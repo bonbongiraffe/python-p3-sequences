c6ad1b487dbe043074c0208c4ef13f6d69ad4bd8
bonbongiraffe <francescowai@yahoo.com>
2023-07-03 09:53:53 -0400

    first attempt

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = [0,1]
+    if length == 0:
+        print ([])
+    elif length == 1:
+        print ([0])
+    elif length == 2:
+        print ([0,1])
+    # why doesn't this work?
+    # if (length >= 1) and (length <= 2):
+    #     for i in range (length):
+    #         print (fibonacci[0:length][i])
+    else:
+        for i in range (length-2):
+            fibonacci.append(fibonacci[i] + fibonacci[i+1])
+        print (fibonacci)
